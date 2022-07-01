989ca5b1e70f54828d81cb8b90739dde95cd06e6
Iyana Marquez <yayakix@gmail.com>
2022-06-30 20:41:50 -0400

    styling projects page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,14 @@ function Footer() {
       <h3 className="connect">LET'S CONNECT</h3>
       <h4>myemail@email.com</h4>
       <h4>
-        <a href="https://www.linkedin.com/in/iyana-marquez/">LINKEDIN</a>
+        <a href="https://www.linkedin.com/in/iyana-marquez/" target="_blank">
+          LINKEDIN
+        </a>
       </h4>
       <h4>
-        <a href="https://github.com/yayakix">GITHUB</a>{" "}
+        <a href="https://github.com/yayakix" target="_blank">
+          GITHUB
+        </a>{" "}
       </h4>
       <span className="copyright">&copy; Iyana Marquez {year} </span>
     </div>
