09c00a7d9c08b0fe4aaf757d5d62c23cdfe2b037
Iyana Marquez <yayakix@gmail.com>
2022-07-07 21:36:21 -0400

    techs added

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -4,16 +4,21 @@ function Technologies() {
   return (
     <div className="tech" id="tech">
       <h2>Technologies</h2>
-      <img
-        className="techimage"
-        src="https://cdn.pixabay.com/photo/2015/04/23/17/41/javascript-736400_960_720.png"
-        alt="me"
-      />
-      <img
-        className="techimage"
-        src="https://cdn.pixabay.com/photo/2015/04/23/17/41/node-js-736399_960_720.png"
-        alt="me"
-      />
+      <div className="techcontainer">
+        <img src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E" />
+        <img src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white" />
+        <img src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white" />
+        <img src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB" />
+        <img src="https://img.shields.io/badge/SASS-hotpink.svg?style=for-the-badge&logo=SASS&logoColor=white" />
+        <img src="https://img.shields.io/badge/jquery-%230769AD.svg?style=for-the-badge&logo=jquery&logoColor=white" />
+        <img src="https://img.shields.io/badge/MUI-%230081CB.svg?style=for-the-badge&logo=mui&logoColor=white" />
+        <img src="https://img.shields.io/badge/NPM-%23000000.svg?style=for-the-badge&logo=npm&logoColor=white" />
+        <img src="https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white" />
+        <img src="https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB" />
+        <img src="https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54" />
+        <img src="https://img.shields.io/badge/django-%23092E20.svg?style=for-the-badge&logo=django&logoColor=white" />
+        {/* <img src="" /> */}
+      </div>
     </div>
   );
 }
