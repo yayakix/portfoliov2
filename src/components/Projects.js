0f9b21a951bd4b1173a099ab139a7791e3761113
Iyana Marquez <yayakix@gmail.com>
2022-07-10 16:51:52 -0400

    redo tech and projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,86 +3,110 @@ import "../css/Projects.css";
 function Projects() {
 
   return (
-    <div className="aboutme">
+    <div className="projectspage">
       <h2 className="projectintro" id="projects">
         Projects
       </h2>
-      <div className="container">
-        <img src="https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
-        <p>
-          <h3>Funky Shoes</h3>
-          This app will show you shoes like no other. Lorem ipsum dolor sit
-          amet, consectetur adipiscing elit. Nunc nisl llis nec lobortis sit
-          amet, volutpat sed tellus. Donec egestas malesuada turpis a malesuada.
-          Aenean auctor ultrices nibh, nec vehicula eros tempus ac.
-          <br></br>
-        </p>
-        <div className="buttoncontainer">
-          <a href="https://funkyshoes.netlify.app/" target="blank">
-            <button class="button-52">Live Site </button>
-          </a>
-          <a
-            href="https://github.com/yayakix/Funky-Shoes-frontend"
-            target="blank"
-          >
-            <button class="button-52">View Code</button>
-          </a>
-        </div>
-      </div>
-
-      <div className="container">
-        <img src="https://images.unsplash.com/photo-1622929611233-bd6d58cbd4dd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80" />
-        <p>
-          <h3>Instagram Clone</h3>A clone of the popular Instagram! I recreated
-          the home and profile pages. Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit. Nunc nislLorem ipsum dolor sit amet, consectetur
-          <br></br>
-        </p>
-        <div className="buttoncontainer">
-          <a href="https://insta-clone33.herokuapp.com/" target="blank">
-            <button class="button-52">Live site </button>
-          </a>
-          <a href="https://github.com/yayakix/instagram-clone" target="blank">
-            <button class="button-52">View Code </button>
-          </a>
-        </div>
-      </div>
-
-      <div className="container">
-        <img src="https://images.pexels.com/photos/6927334/pexels-photo-6927334.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-        <p>
-          <h3>calculator</h3>A calculator built with React. Built to have a
-          similar vibe to the one on your phone. Performs basic four functions
-          and handles percents and negative numbers. Lorem ipsum dolor sit amet,
-          <br></br>
-        </p>
-        <div className="buttoncontainer">
-          <a href="https://simplecalculator-7d4924.netlify.app/" target="blank">
-            <button class="button-52">Live Site</button>
-          </a>
-          <a href="https://github.com/yayakix/calculator-react" target="blank">
-            <button class="button-52">View Code </button>
-          </a>
-        </div>
-      </div>
-
-      <div className="container">
-        <img src="https://images.pexels.com/photos/209728/pexels-photo-209728.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-        <p>
-          <h3>Simon Says Game</h3>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nisl
-          sem, mollis nec lobortis sit amet, volutpat sed tellus. Donec egestas
-          malesuada turpis a malesuada. Aenean auctor ultrices nibh, nec
-          vehicula eros tempus ac.
-          <br></br>
-        </p>
-        <div className="buttoncontainer">
-          <a href="https://simonsaysgame23422.netlify.app/" target="blank">
-            <button class="button-52">Live Site </button>
-          </a>
-          <a href="https://github.com/yayakix/simonsays" target="blank">
-            <button class="button-52">View Code</button>
-          </a>
+      <div class="container overflow-hidden">
+        <div class="row gy-5">
+          <div class="col-6">
+            <div class="p-3 border bg-light">
+              <img src="https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
+              <p>
+                <h3>Funky Shoes</h3>
+                This app will show you shoes like no other. Lorem ipsum dolor
+                sit amet, consectetur adipiscing elit. Nunc nisl llis nec
+                lobortis sit amet, volutpat sed tellus. Donec egestas malesuada
+                turpis a malesuada. Aenean auctor ultrices nibh, nec vehicula
+                eros tempus ac.
+                <br></br>
+              </p>
+              <div className="buttoncontainer">
+                <a href="https://funkyshoes.netlify.app/" target="blank">
+                  <button class="button-52">Live Site </button>
+                </a>
+                <a
+                  href="https://github.com/yayakix/Funky-Shoes-frontend"
+                  target="blank"
+                >
+                  <button class="button-52">View Code</button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="p-3 border bg-light">
+              <img src="https://images.unsplash.com/photo-1622929611233-bd6d58cbd4dd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80" />
+              <p>
+                <h3>Instagram Clone</h3>A clone of the popular Instagram! I
+                recreated the home and profile pages. Lorem ipsum dolor sit
+                amet, consectetur adipiscing elit. Nunc nislLorem ipsum dolor
+                sit amet, consectetur
+                <br></br>
+              </p>
+              <div className="buttoncontainer">
+                <a href="https://insta-clone33.herokuapp.com/" target="blank">
+                  <button class="button-52">Live site </button>
+                </a>
+                <a
+                  href="https://github.com/yayakix/instagram-clone"
+                  target="blank"
+                >
+                  <button class="button-52">View Code </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="p-3 border bg-light">
+              {" "}
+              <img src="https://images.pexels.com/photos/6927334/pexels-photo-6927334.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+              <p>
+                <h3>calculator</h3>This is a calculator that performs simple
+                math operations. It performs the 4 basic operations in addition to handling percents and negative numbers. This app was created with functional components, state and hooks.
+                
+                <br></br>
+              </p>
+              <div className="buttoncontainer">
+                <a
+                  href="https://simplecalculator-7d4924.netlify.app/"
+                  target="blank"
+                >
+                  <button class="button-52">Live Site</button>
+                </a>
+                <a
+                  href="https://github.com/yayakix/calculator-react"
+                  target="blank"
+                >
+                  <button class="button-52">View Code </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="p-3 border bg-light">
+              <img src="https://images.pexels.com/photos/209728/pexels-photo-209728.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+              <p>
+                <h3>Simon Says Game</h3>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                nisl sem, mollis nec lobortis sit amet, volutpat sed tellus.
+                Donec egestas malesuada turpis a malesuada. Aenean auctor
+                ultrices nibh, nec vehicula eros tempus ac.
+                <br></br>
+              </p>
+              <div className="buttoncontainer">
+                <a
+                  href="https://simonsaysgame23422.netlify.app/"
+                  target="blank"
+                >
+                  <button class="button-52">Live Site </button>
+                </a>
+                <a href="https://github.com/yayakix/simonsays" target="blank">
+                  <button class="button-52">View Code</button>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -2,24 +2,57 @@ import "../css/Tech.css";
 
 function Technologies() {
   return (
-    <div className="tech" id="tech">
+    <div className="techpage" id="tech">
       <h2 className="techtitle">Technologies</h2>
-      <div className="techcontainer">
-        <img src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E" />
-        <img src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white" />
-        <img src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white" />
-        <img src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB" />
-        <img src="https://img.shields.io/badge/SASS-hotpink.svg?style=for-the-badge&logo=SASS&logoColor=white" />
-        <img src="https://img.shields.io/badge/jquery-%230769AD.svg?style=for-the-badge&logo=jquery&logoColor=white" />
-        <img src="https://img.shields.io/badge/MUI-%230081CB.svg?style=for-the-badge&logo=mui&logoColor=white" />
-        <img src="https://img.shields.io/badge/NPM-%23000000.svg?style=for-the-badge&logo=npm&logoColor=white" />
-        <img src="https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white" />
-        <img src="https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB" />
-        <img src="https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54" />
-        <img src="https://img.shields.io/badge/django-%23092E20.svg?style=for-the-badge&logo=django&logoColor=white" />
-        <img src="https://img.shields.io/badge/Firebase-039BE5?style=for-the-badge&logo=Firebase&logoColor=white" />
+      <div className="techcontainer container">
+          <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+            <div class="col">
+              <div class="p-3 border bg-light">
+                {" "}
+                <i class="devicon-react-original-wordmark"></i>
+              </div>
+            </div>
+            <div class="col">
+              <div class="p-3 border bg-light">
+                {" "}
+                <i class="devicon-javascript-plain"></i>
+              </div>
+            </div>
+            <div class="col">
+              <div class="p-3 border bg-light">
+                <i class="devicon-html5-plain-wordmark"></i>
+              </div>
+            </div>
+            <div class="col">
+              <div class="p-3 border bg-light">
+                {" "}
+                <i class="devicon-css3-plain-wordmark"></i>
+              </div>
+            </div>
+            <div class="col">
+              <div class="p-3 border bg-light">
+                {" "}
+                <i class="devicon-python-plain-wordmark"></i>
+              </div>
+            </div>
+            <div class="col">
+              <div class="p-3 border bg-light">Row column</div>
+            </div>
+            <div class="col">
+              <div class="p-3 border bg-light">Row column</div>
+            </div>
+            <div class="col">
+              <div class="p-3 border bg-light">Row column</div>
+            </div>
+            <div class="col">
+              <div class="p-3 border bg-light">Row column</div>
+            </div>
+            <div class="col">
+              <div class="p-3 border bg-light">Row column</div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
   );
 }
 
