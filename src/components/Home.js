30c1124361c2a530717a5413f0ee3cbcac55a700
Iyana Marquez <yayakix@gmail.com>
2022-07-10 19:28:48 -0400

    homepage edit

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,20 +4,23 @@ function Home() {
   return (
     <div class="container homepage">
       <div class="row">
-        <div class="col-6">
+        <div class="col-sm-12 col-md-6 homeintro">
           {" "}
           <h1 className="text" id="home">
-            Hi. I'm Iyana<span className="hand">👋🏽</span>
+            Hi. I'm Iyana<span className="hand wave">👋🏽</span>
           </h1>
           <p>
             Im a <span className="spectext"> Frontend </span> Developer
           </p>
         </div>
-      </div>
-      <div class="col-6">
-        <img src="https://i.imgur.com/x7wCu69.jpg" />
+        <div class="col-sm-12 col-md-6">
+          {" "}
+          <img src="https://i.imgur.com/x7wCu69.jpg" />
+        </div>
       </div>
     </div>
+
+    // new stuff below
   );
 }
 
