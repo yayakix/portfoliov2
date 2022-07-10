e35f7e9e53cc657d246c51b89c0be53e0474c8c0
Iyana Marquez <yayakix@gmail.com>
2022-07-10 17:04:57 -0400

    resize projects for mobile

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,7 +9,7 @@ function Projects() {
       </h2>
       <div class="container overflow-hidden">
         <div class="row gy-5">
-          <div class="col-6">
+          <div class="col-sm-12 col-md-6 ">
             <div class="p-3 border bg-light">
               <img src="https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
               <p>
@@ -34,7 +34,7 @@ function Projects() {
               </div>
             </div>
           </div>
-          <div class="col-6">
+          <div class="col-sm-12 col-md-6">
             <div class="p-3 border bg-light">
               <img src="https://images.unsplash.com/photo-1622929611233-bd6d58cbd4dd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80" />
               <p>
@@ -57,14 +57,15 @@ function Projects() {
               </div>
             </div>
           </div>
-          <div class="col-6">
+          <div class="col-sm-12 col-md-6 ">
             <div class="p-3 border bg-light">
               {" "}
               <img src="https://images.pexels.com/photos/6927334/pexels-photo-6927334.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
               <p>
                 <h3>calculator</h3>This is a calculator that performs simple
-                math operations. It performs the 4 basic operations in addition to handling percents and negative numbers. This app was created with functional components, state and hooks.
-                
+                math operations. It performs the 4 basic operations in addition
+                to handling percents and negative numbers. This app was created
+                with functional components, state and hooks.
                 <br></br>
               </p>
               <div className="buttoncontainer">
@@ -83,7 +84,7 @@ function Projects() {
               </div>
             </div>
           </div>
-          <div class="col-6">
+          <div class="col-sm-12 col-md-6 ">
             <div class="p-3 border bg-light">
               <img src="https://images.pexels.com/photos/209728/pexels-photo-209728.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
               <p>
