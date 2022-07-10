229a08c935a50696e1da589f4ed94aca60c326b3
Iyana Marquez <yayakix@gmail.com>
2022-07-10 16:56:09 -0400

    tech page height change

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -5,54 +5,69 @@ function Technologies() {
     <div className="techpage" id="tech">
       <h2 className="techtitle">Technologies</h2>
       <div className="techcontainer container">
-          <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
-            <div class="col">
-              <div class="p-3 border bg-light">
-                {" "}
-                <i class="devicon-react-original-wordmark"></i>
-              </div>
+        <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+          <div class="col">
+            <div class="p-3 border bg-light">
+              {" "}
+              <i class="devicon-react-original-wordmark"></i>
             </div>
-            <div class="col">
-              <div class="p-3 border bg-light">
-                {" "}
-                <i class="devicon-javascript-plain"></i>
-              </div>
+          </div>
+          <div class="col">
+            <div class="p-3 border bg-light">
+              {" "}
+              <i class="devicon-javascript-plain"></i>
             </div>
-            <div class="col">
-              <div class="p-3 border bg-light">
-                <i class="devicon-html5-plain-wordmark"></i>
-              </div>
+          </div>
+          <div class="col">
+            <div class="p-3 border bg-light">
+              <i class="devicon-html5-plain-wordmark"></i>
             </div>
-            <div class="col">
-              <div class="p-3 border bg-light">
-                {" "}
-                <i class="devicon-css3-plain-wordmark"></i>
-              </div>
+          </div>
+          <div class="col">
+            <div class="p-3 border bg-light">
+              {" "}
+              <i class="devicon-css3-plain-wordmark"></i>
             </div>
-            <div class="col">
-              <div class="p-3 border bg-light">
-                {" "}
-                <i class="devicon-python-plain-wordmark"></i>
-              </div>
+          </div>
+          <div class="col">
+            <div class="p-3 border bg-light">
+              {" "}
+              <i class="devicon-python-plain-wordmark"></i>
             </div>
-            <div class="col">
-              <div class="p-3 border bg-light">Row column</div>
-            </div>
-            <div class="col">
-              <div class="p-3 border bg-light">Row column</div>
-            </div>
-            <div class="col">
-              <div class="p-3 border bg-light">Row column</div>
-            </div>
-            <div class="col">
-              <div class="p-3 border bg-light">Row column</div>
-            </div>
-            <div class="col">
-              <div class="p-3 border bg-light">Row column</div>
-            </div>
+          </div>
+          <div class="col">
+            <div class="p-3 border bg-light">Row column</div>
+          </div>
+          <div class="col">
+            <div class="p-3 border bg-light">Row column</div>
+          </div>
+          <div class="col">
+            <div class="p-3 border bg-light">Row column</div>
+          </div>
+          <div class="col">
+            <div class="p-3 border bg-light">Row column</div>
+          </div>
+          <div class="col">
+            <div class="p-3 border bg-light">Row column</div>
+          </div>
+          <div class="col">
+            <div class="p-3 border bg-light">Row column</div>
+          </div>
+          <div class="col">
+            <div class="p-3 border bg-light">Row column</div>
+          </div>
+          <div class="col">
+            <div class="p-3 border bg-light">Row column</div>
+          </div>
+          <div class="col">
+            <div class="p-3 border bg-light">Row column</div>
+          </div>
+          <div class="col">
+            <div class="p-3 border bg-light">Row column</div>
           </div>
         </div>
       </div>
+    </div>
   );
 }
 
