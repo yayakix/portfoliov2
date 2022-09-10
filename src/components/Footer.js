170542dd6ff88198db556c6109c7b4fbf35f4d89
Iyana Marquez <yayakix@gmail.com>
2022-09-10 14:42:41 -0400

    footer push

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,26 +7,28 @@ function Footer() {
     <footer className="footer">
       <span className="foottitle text-center">Lets Connect.</span>
 
-      <div className="group flex row justify-start">
-        {/* <a className="link link-hover">Branding</a> */}
-        <a href="https://github.com/yayakix" target="_blank">
+      <div className="group flex row  ">
+        <a href="https://github.com/yayakix" target="_blank" rel="noreferrer">
           Github
         </a>
-        <a href="https://www.linkedin.com/in/iyana-marquez/" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/iyana-marquez/"
+          target="_blank"
+          rel="noreferrer"
+        >
           LinkedIn
         </a>
         <a
           className="link link-hover"
           target="_blank"
+          rel="noreferrer"
           href="https://docs.google.com/document/d/10DHq4Xq6HKmtkbnLC8-JJA_4nqbkPZ-RNV2bcQ6QnhQ/edit?usp=sharing"
         >
           Resume
         </a>
       </div>
 
-      <span className="copyright ">
-        &copy; Iyana Marquez {year}{" "}
-      </span>
+      <span className="copyright ">&copy; Iyana Marquez {year} </span>
     </footer>
     // sep
   );
