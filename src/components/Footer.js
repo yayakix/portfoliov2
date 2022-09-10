cffb33028d9c8527f66fabf88e5b7b3ae2eab187
Iyana Marquez <yayakix@gmail.com>
2022-09-10 14:32:02 -0400

    changed footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,30 +4,31 @@ function Footer() {
     const d = new Date()
     const year = d.getFullYear()
   return (
-    <div className="footer" id="contact">
-      <h3 className="connect">LET'S CONNECT</h3>
-      <h4>Iyana Marquez</h4>
-      {/* <h4>yayakix@gmail.com</h4> */}
-      <h4>
-        <a
-          href="https://docs.google.com/document/d/10DHq4Xq6HKmtkbnLC8-JJA_4nqbkPZ-RNV2bcQ6QnhQ/edit?usp=sharing"
-          target="_blank"
-        >
-          RESUME
-        </a>
-      </h4>
-      <h4>
-        <a href="https://www.linkedin.com/in/iyana-marquez/" target="_blank">
-          LINKEDIN
-        </a>
-      </h4>
-      <h4>
+    <footer className="footer">
+      <span className="foottitle text-center">Lets Connect.</span>
+
+      <div className="group flex row justify-start">
+        {/* <a className="link link-hover">Branding</a> */}
         <a href="https://github.com/yayakix" target="_blank">
-          GITHUB
-        </a>{" "}
-      </h4>
-      <span className="copyright">&copy; Iyana Marquez {year} </span>
-    </div>
+          Github
+        </a>
+        <a href="https://www.linkedin.com/in/iyana-marquez/" target="_blank">
+          LinkedIn
+        </a>
+        <a
+          className="link link-hover"
+          target="_blank"
+          href="https://docs.google.com/document/d/10DHq4Xq6HKmtkbnLC8-JJA_4nqbkPZ-RNV2bcQ6QnhQ/edit?usp=sharing"
+        >
+          Resume
+        </a>
+      </div>
+
+      <span className="copyright ">
+        &copy; Iyana Marquez {year}{" "}
+      </span>
+    </footer>
+    // sep
   );
 }
 
